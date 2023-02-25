ffcaf84f9619200e7755ce303fa0e6972744cc61
Promit-revar <promit.revar2211@gmail.com>
2023-02-25 18:00:35 +0530

    Feat: Style Components

--- record_self/src/App.js
+++ record_self/src/App.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React from "react"; 
+import "./assets/style.css";
 
 import Card from "./components/CardComponent/CardComponent";
 
--- record_self/src/components/RecordCardComponent/RecordCardComponent.jsx
+++ record_self/src/components/RecordCardComponent/RecordCardComponent.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable */
 import React from 'react';
 import './RecordCardComponent.css';
+import heart_red from '../../assets/heart-red.svg';
 export default function RecordCardComponent(props){
     
     return(
@@ -8,6 +9,20 @@ export default function RecordCardComponent(props){
             <div className='record-image'>
                 <img src={props.data.imageUrl} alt="props.name"/>
             </div>
+            <div className='record-title-bar'>
+                
+                    <div className='record-title'>
+                        <p>{props.data.name}</p>
+                    </div>
+                
+                <div className='heart'>
+                    <p>2</p>
+                    <img src={heart_red} alt='heart_red'/>
+                </div>
+            </div>
+            <div className='record-artist'>
+                <strong>{props.data.artist.name}</strong> 
+            </div> 
             
         </div>
     )
